3dbc90b6416db98da668d4589cb2171c504f1a79
prajwal124567 <thepajjukoli@gmail.com>
2025-03-12 15:39:36 +0530

    Create costum.py

---
+++ costum.py
@@ -0,0 +1,9 @@
+# main.py
+
+import math_operations  # Importing the custom module
+
+# Using the functions
+print("Addition:", math_operations.add(10, 5))
+print("Subtraction:", math_operations.subtract(10, 5))
+print("Multiplication:", math_operations.multiply(10, 5))
+print("Division:", math_operations.divide(10, 5))
